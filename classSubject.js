cf757ca044f72c85d964d2f0846624e5bec58930
warren-glitch <warrenpiano24@gmail.com>
2024-11-04 20:11:35 +0800

    initial commit

---
+++ classSubject.js
@@ -0,0 +1,7 @@
+let subjectTitle = "Data Structures and Algorithms"
+let classSchedule = "8:00-9:30 in Monday and Friday"
+let classroom = "Computer Laboratory A"
+let classInstructor = "Mr. Vitruvius John Barayuga"
+let studentName = "Warren M. Piano"
+
+console.log('${studentName} is currently enrolled in ${subjectTitle} with a class schedule of ${classSchedule} at room ${classroom}. The instructor for the subject is ${classInstructor}'.);
